f1f9ca5b1353d1fe2fd84481fead29a86f8b1853
Stone Preston <stone.preston@canonical.com>
2023-03-09 09:56:16 -0600

    Refactor actions enqueue function to accept input struct instead of action itself

--- juju/actions.go
+++ juju/actions.go
@@ -10,20 +10,32 @@ type actionsClient struct {
 	ConnectionFactory
 }
 
+type EnqueueOperationInput struct {
+	Receiver   string
+	Name       string
+	Parameters map[string]interface{}
+}
+
 func newActionsClient(cf ConnectionFactory) *actionsClient {
 	return &actionsClient{
 		ConnectionFactory: cf,
 	}
 }
 
-func (c *actionsClient) EnqueueOperation(ctx context.Context, actions []action.Action, modelUUID string) (action.EnqueuedActions, error) {
+func (c *actionsClient) EnqueueOperation(ctx context.Context, input EnqueueOperationInput, modelUUID string) (action.EnqueuedActions, error) {
 	conn, err := c.GetConnection(ctx, &modelUUID)
 	if err != nil {
 		return action.EnqueuedActions{}, err
 	}
 	client := action.NewClient(conn)
 
-	return client.EnqueueOperation(actions)
+	act := action.Action{
+		Receiver:   input.Receiver,
+		Name:       input.Name,
+		Parameters: input.Parameters,
+	}
+	acts := []action.Action{act}
+	return client.EnqueueOperation(acts)
 }
 
 func (c *actionsClient) GetOperation(ctx context.Context, operationID string, modelUUID string) (action.Operation, error) {
